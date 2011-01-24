a993432fef38854b90562692d0d5a348a1f266a4
Max Gonzih <gonzih@gmail.com>
2011-01-24 23:55:04 +0200

    DRY in helper

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -2,12 +2,12 @@ helpers do
   def build_files_list hash, key = '/'
     output = "<a class='dir' href='#'><img src='/images/folder.png'>#{key}</a><br /><div class='level'>"
     if hash.class == Hash
-      hash.each_key do |key|
-        if hash[key].class == Hash
-          output += build_files_list hash[key], key
+      hash.each do |key, item|
+        if item.class == Hash
+          output += build_files_list item, key
         else
-          if hash[key].class == String
-            output += "<a class='file' href='#{hash[key]}'><img src='/images/mplayer.png' >#{key}</a><br />"
+          if item.class == String
+            output += "<a class='file' href='#{item}'><img src='/images/mplayer.png' >#{key}</a><br />"
           end
         end
       end
